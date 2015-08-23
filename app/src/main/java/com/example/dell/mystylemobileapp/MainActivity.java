07872743d34cfe6083e9853ab9a2398e7ae8954a
kusalhp <kusalhp@gmail.com>
2015-08-23 18:05:24 +0530

    temperory splash screen

--- app/src/main/java/com/example/dell/mystylemobileapp/MainActivity.java
+++ app/src/main/java/com/example/dell/mystylemobileapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dell.mystylemobileapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +13,33 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),Signin.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
+
+
+
     }
 
 
